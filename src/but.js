c5823b71fa0cb86d28bce55670ba6b99e93ee439
Naveen R <naveen555ravi@gmail.com>
2024-09-02 23:06:57 +0530

    first commit

---
+++ src/but.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function Button({handle,children}) {
+    console.log('rendering',children);
+
+  return (
+    <button onClick={handle}>
+{children}
+    </button>
+  )
+}
+
+export default React.memo(Button)
